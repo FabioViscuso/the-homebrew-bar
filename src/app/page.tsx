e93a726088ff4eef94328234b02ee443d7e614d4
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-20 23:47:12 +0200

    fix: better typography responiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const sacramento = Sacramento({ subsets: ["latin"], weight: "400" });
 export default function Landing() {
   return (
     <main
-      className={` bg-wall overflow-y-scroll snap-y snap-mandatory flex h-screen flex-col lg:flex-row items-center justify-between `}
+      className={` bg-wall overflow-y-scroll snap-y snap-mandatory flex h-screen flex-col lg:flex-row items-center justify-between select-none `}
     >
       <div
         className={
@@ -38,22 +38,22 @@ export default function Landing() {
       </div>
       <div
         className={
-          " snap-start shrink-0 grow-0 basis-full lg:basis-[35vw] h-[100svh] w-full [text-shadow:0px_0px_2px_#fff] p-6 bg-black bg-opacity-20 [backdrop-filter:blur(2px)] flex flex-col "
+          " snap-start shrink-0 grow-0 basis-full lg:basis-[35vw] h-[100svh] w-full [text-shadow:0px_0px_2px_#fff] py-8 px-8 bg-black bg-opacity-20 [backdrop-filter:blur(2px)] flex flex-col "
         }
       >
-        <p className=" text-2xl xl:text-3xl ">
+        <p className=" text-2xl md:text-3xl lg:text-2xl xl:text-3xl ">
           Expecting guests? Want to impress friends or surprise your significant
           other for a special occasion? Or, why not dedicate a moment just for
           yourself?
         </p>
-        <p className=" text-2xl mt-6 xl:text-3xl ">
+        <p className="  mt-6 text-2xl md:text-3xl lg:text-2xl xl:text-3xl ">
           Embark on a journey to become an experienced barman, and unlock the
           secrets of cocktail making, in the comfort of your home or on the go.
         </p>
-        <p className=" text-2xl mt-6 xl:text-3xl ">
-          And the best thing: it’s free!
+        <p className=" mt-6 text-2xl md:text-3xl lg:text-2xl xl:text-3xl ">
+          Discover new recipes, save them for later, get inspired. For free!
         </p>
-        <Link href={'/dashboard'} className={`${sacramento.className} text-center text-3xl md:text-5xl lg:text-6xl mt-auto neon-button-border neon-button-red `}>
+        <Link href={'/dashboard'} className={`${sacramento.className} text-center text-3xl md:text-5xl lg:text-4xl xl:text-6xl mt-auto neon-button-border neon-button-red `}>
           Let's start mixing!
         </Link>
       </div>
