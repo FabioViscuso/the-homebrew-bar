9d63ed0bd8bb8db32077ca2f31b6853f09830cd0
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-20 23:18:46 +0200

    feat: add button link to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Poiret_One, Sacramento } from "next/font/google";
 import Image from "next/image";
 import { icons } from "@/lib/iconsImport";
+import Link from "next/link";
 
 const poiret = Poiret_One({ subsets: ["latin"], weight: "400" });
 const sacramento = Sacramento({ subsets: ["latin"], weight: "400" });
@@ -30,14 +31,14 @@ export default function Landing() {
           }
         />
         <p
-          className={`${sacramento.className} hero-sign [font-size:clamp(1rem,2vmin+1rem,5rem)] neon-icon-flicker bottom-44 md:bottom-22 lg:bottom-18 xl:bottom-24 right-10 md:right-32 lg:-right-12 `}
+          className={`${sacramento.className} hero-sign [font-size:clamp(1rem,2vmin+1rem,5rem)] neon-button-border neon-button-pink neon-icon-flicker bottom-44 md:bottom-22 lg:bottom-18 xl:bottom-24 right-10 md:right-32 lg:-right-12 `}
         >
           Always open
         </p>
       </div>
       <div
         className={
-          " snap-start shrink-0 grow-0 basis-full lg:basis-[35vw] h-[100svh] w-full [text-shadow:0px_0px_2px_#fff] p-6 bg-black bg-opacity-20 [backdrop-filter:blur(2px)] "
+          " snap-start shrink-0 grow-0 basis-full lg:basis-[35vw] h-[100svh] w-full [text-shadow:0px_0px_2px_#fff] p-6 bg-black bg-opacity-20 [backdrop-filter:blur(2px)] flex flex-col "
         }
       >
         <p className=" text-2xl xl:text-3xl ">
@@ -52,6 +53,9 @@ export default function Landing() {
         <p className=" text-2xl mt-6 xl:text-3xl ">
           And the best thing: it’s free!
         </p>
+        <Link href={'/dashboard'} className={`${sacramento.className} text-center text-6xl mt-auto neon-button-border neon-button-red `}>
+          Let's start mixing!
+        </Link>
       </div>
     </main>
   );
