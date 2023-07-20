b7ec8e9c5a3a04d80281902434b7c37f12fbba44
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-20 19:32:49 +0200

    feat: initial centralized icons import

---
+++ src/lib/iconsImport.ts
@@ -0,0 +1,9 @@
+import cocktailIcon from '../../public/icons/cocktail.png';
+import shakerIcon from '../../public/icons/shaker.png';
+import jiggerIcon from '../../public/icons/jigger.png';
+
+export const icons = {
+    cocktailIcon,
+    shakerIcon,
+    jiggerIcon
+}
