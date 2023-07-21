d228ca337b10ef096df906980103ab6457fc2908
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-21 15:26:26 +0200

    chore: minor dependencies reordering

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
-import { icons } from "@/lib/iconsImport";
-import DashboardLink from "../../components/dashboard/DashboardLink";
-import Explore from "../../components/dashboard/views/Explore";
-import Favorites from "../../components/dashboard/views/Favorites";
-import About from "../../components/dashboard/views/About";
-
-import { Poiret_One, Sacramento } from "next/font/google";
-import HamburgerMenuModal from "@/components/dashboard/HamburgerMenuModal";
 import { createPortal } from "react-dom";
-const poiret = Poiret_One({ subsets: ["latin"], weight: "400" });
+import Image from "next/image";
+
+import DashboardLink from "@/components/dashboard/DashboardLink";
+import Explore from "@/components/dashboard/views/Explore";
+import Favorites from "@/components/dashboard/views/Favorites";
+import About from "@/components/dashboard/views/About";
+import HamburgerMenuModal from "@/components/dashboard/HamburgerMenuModal";
+
+import { icons } from "@/lib/iconsImport";
+import { Sacramento } from "next/font/google";
 const sacramento = Sacramento({ subsets: ["latin"], weight: "400" });
 
 export enum Views {
@@ -39,7 +39,7 @@ export default function Landing() {
 
   return (
     <main id="application" className=" bg-wall flex min-h-screen flex-col items-center justify-between p-24 ">
-      <nav className={`${sacramento.className} fixed top-0 left-0 right-0 h-20 px-6 flex justify-between items-center border-b-4 [box-shadow:0px_0px_200px_1px_#aaa] bg-black bg-opacity-40 [backdrop-filter:blur(4px)] `}>
+      <nav className={`${sacramento.className} fixed top-0 left-0 right-0 h-20 px-6 flex justify-between items-center border-b-4 [box-shadow:0px_0px_200px_1px_#aaa] bg-black bg-opacity-40 [backdrop-filter:blur(4px)] select-none `}>
         <div className="flex gap-5 items-center ">
             <h1 className={`${sacramento.className} text-4xl neon-blue mt-2 `}>The Homebrew Bar</h1>
             <Image src={icons.cocktailIcon} alt="" width={128} className=" w-12 neon-button-pink " />
