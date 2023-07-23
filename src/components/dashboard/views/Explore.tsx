ed6cfa689a08f55b1166c47f2bc8ba58ccefe590
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-23 16:38:09 +0200

    feat: improve mobile experience
    the whole input group is on one scrollable line

--- src/components/dashboard/views/Explore.tsx
+++ src/components/dashboard/views/Explore.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, MutableRefObject, useEffect, useRef } from "react";
 import useCocktailsStore from "@/store/store";
 
-
 import { Poiret_One, Sacramento } from "next/font/google";
 import CardScroller from "../CardScroller";
 const poiret = Poiret_One({ subsets: ["latin"], weight: "400" });
@@ -68,32 +67,48 @@ export default function Explore() {
 
   return (
     <section
-      className={`${sacramento.className} h-full w-full mt-20 pt-10 flex flex-col items-center justify-between gap-5 overflow-x-hidden `}
+      className={`${sacramento.className} h-full w-full mt-20 flex flex-col items-center justify-between gap-5 overflow-x-hidden `}
     >
-      <div className=" w-full flex flex-col md:flex-row justify-center items-center gap-6 md:gap-12 lg:gap-24">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <input
-            ref={inputValueRef}
-            className={`${poiret.className} text-lg lg:text-2xl bg-black bg-opacity-40 [backdrop-filter:blur(4px)] text-[#fff] placeholder:text-white rounded-md w-[clamp(5rem,30vmax,30rem)] neon-button-border [box-shadow:0px_0px_40px_1px_#888] `}
-            type="text"
-            placeholder="Type here, try 'Mojito'"
-          />
-          <button type="submit" className="text:2xl md:text-3xl lg:text-6xl neon-button-red ">
-            Search
+      <div id="explore-input-group" className=" pt-10 w-screen lg:w-full overflow-x-scroll snap-x snap-mandatory lg:overflow-hidden flex flex-row flex-nowrap lg:justify-center items-center gap-6 md:gap-12 lg:gap-24 ">
+        <div className=" w-screen lg:w-auto snap-start grow-0 shrink-0 flex items-center gap-6 md:gap-12 lg:gap-24 px-12 pb-10 lg:px-0 lg:pb-0 ">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-full ">
+            <input
+              ref={inputValueRef}
+              className={`${poiret.className} text-lg lg:text-2xl bg-black bg-opacity-40 [backdrop-filter:blur(4px)] text-[#fff] placeholder:text-white rounded-md lg:w-[clamp(5rem,30vmax,30rem)] neon-button-border [box-shadow:0px_0px_40px_1px_#888] `}
+              type="text"
+              placeholder="Type here, try 'Mojito'"
+            />
+            <button
+              type="submit"
+              className=" text-4xl lg:text-6xl neon-button-red "
+            >
+              Search
+            </button>
+          </form>
+
+          <span className=" text-5xl hidden lg:block ">
+            Or
+          </span>
+          <p className=" text-4xl flex lg:hidden flex-col items-center [text-shadow:0px_0px_2px_#fff] ">
+            Or <span className=" [text-shadow:0px_0px_4px_#fff] ">⇨</span>
+          </p>
+        </div>
+
+        <div className=" w-screen lg:w-auto snap-start grow-0 shrink-0 flex justify-center items-center ">
+          <button
+            onClick={handleRandomValueSubmit}
+            role="button"
+            className=" text-5xl lg:text-6xl neon-button-red neon-button-border "
+          >
+            Surprise me!
           </button>
-        </form>
-        <span className="text:2xl md:text-3xl lg:text-5xl">Or</span>
-        <button
-          onClick={handleRandomValueSubmit}
-          role="button"
-          className="text:2xl md:text-3xl lg:text-6xl neon-button-red neon-button-border "
-        >
-          Surprise me!
-        </button>
+        </div>
       </div>
       <div>
-      <p className={`${poiret.className} text-lg ml-1 mb-2`}>Drag around to scroll</p>
-      <CardScroller cocktails={cocktails} />
+        <p className={`${poiret.className} text-lg ml-1 mb-2`}>
+          Drag around to scroll
+        </p>
+        <CardScroller cocktails={cocktails} />
       </div>
       <div className=" w-full h-0 border-b-4 border-white [box-shadow:1px_0px_200px_10px_#fff]"></div>
     </section>
