99085ea5d32a918137264fb14bf68519c4127d81
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-23 21:36:40 +0200

    feat: add serach favorites

--- src/components/dashboard/views/Favorites.tsx
+++ src/components/dashboard/views/Favorites.tsx
@@ -1,51 +1,72 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CardScroller from "../CardScroller";
+import useCocktailsStore from "@/store/store";
 
 import { Cocktail } from "@/lib/types/CocktailObj";
 
-import { Sacramento } from "next/font/google";
-import useCocktailsStore from "@/store/store";
+import { Poiret_One, Sacramento } from "next/font/google";
+const poiret = Poiret_One({ subsets: ["latin"], weight: "400" });
 const sacramento = Sacramento({ subsets: ["latin"], weight: "400" });
 
 export default function Favorites() {
-  const addToFavs = useCocktailsStore(state => state.addToFavorites)
-  const removeAllFavs = useCocktailsStore(state => state.clearAllFavorites)
-  const favCocktails = useCocktailsStore(state => state.favoriteCocktails);
-  
+  const [searchString, setSearchString] = useState("");
+  const addToFavs = useCocktailsStore((state) => state.addToFavorites);
+  const removeAllFavs = useCocktailsStore((state) => state.clearAllFavorites);
+
   const handleFavoritesCleanup = (
     event: React.MouseEvent<HTMLButtonElement>
   ) => {
     event.stopPropagation();
 
-    localStorage.setItem("favorites", JSON.stringify([]));
     removeAllFavs();
+    localStorage.setItem("favorites", JSON.stringify([]));
   };
 
-  const locals: Cocktail[] = JSON.parse(localStorage.getItem('favorites') as string)
+  const localFavorites: Cocktail[] = JSON.parse(
+    localStorage.getItem("favorites") as string
+  );
   useEffect(() => {
-    locals.forEach((cocktail) => addToFavs(cocktail))
+    localFavorites.forEach((cocktail) => addToFavs(cocktail));
   }, []);
+
+  const filteredLocalFavorites = localFavorites.filter((cocktail) =>
+    cocktail.strDrink.toLowerCase().includes(searchString.toLowerCase())
+  );
 
   return (
     <section
       className={`${sacramento.className} h-full w-full mt-20 pt-10 flex flex-col items-center justify-between gap-5 overflow-x-hidden `}
     >
-      {locals && locals.length > 0 && (
-        <button
-          role="button"
-          onClick={handleFavoritesCleanup}
-          className=" text-2xl md:text4xl lg:text-6xl neon-button-red neon-button-border "
-        >
-          Clear Favorites
-        </button>
+      {localFavorites && localFavorites.length > 0 && (
+        <div className="flex gap-10">
+          <input
+            type="text"
+            placeholder="Nice list! Search from your favorites"
+            value={searchString}
+            onChange={(e) => setSearchString(e.target.value)}
+            className={`${poiret.className} text-lg lg:text-2xl bg-black bg-opacity-40 [backdrop-filter:blur(4px)] text-[#fff] placeholder:text-white rounded-md lg:w-[clamp(5rem,30vmax,30rem)] neon-button-border [box-shadow:0px_0px_40px_1px_#888] `}
+          />
+          <button
+            role="button"
+            onClick={handleFavoritesCleanup}
+            className=" text-2xl md:text4xl lg:text-4xl neon-button-red neon-button-border "
+          >
+            Clear Favorites
+          </button>
+        </div>
       )}
-      {locals && locals.length > 0 ? (
-        <CardScroller cocktails={locals} />
-      ) : (
-        <h2 className={` text-3xl md:text-4xl lg:text-6xl neon-blue `}>
-          No favorites! Explore some more
-        </h2>
-      )}
+      {localFavorites &&
+        localFavorites.length > 0 &&
+        searchString.length === 0 && (
+          <CardScroller cocktails={localFavorites} />
+        )}
+      {filteredLocalFavorites &&
+        filteredLocalFavorites.length > 0 &&
+        searchString.length > 0 && (
+          <CardScroller cocktails={filteredLocalFavorites} />
+        )}
+      {localFavorites.length === 0  && <h2>No Favorites</h2>}
+      {filteredLocalFavorites.length === 0 && searchString.length > 0 && <h2 className="text-6xl neon-red">No Match</h2>}
       <div className=" w-full h-0 border-b-4 border-white [box-shadow:1px_0px_200px_10px_#fff]"></div>
     </section>
   );
