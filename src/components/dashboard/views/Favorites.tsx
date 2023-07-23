11b174cf7fde0447420e10257125bf5c144c5a92
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-07-23 13:55:54 +0200

    feat: hide clear all button when favorites are empty

--- src/components/dashboard/views/Favorites.tsx
+++ src/components/dashboard/views/Favorites.tsx
@@ -31,16 +31,22 @@ export default function Favorites() {
     <section
       className={`${sacramento.className} h-full w-full mt-20 pt-10 flex flex-col items-center justify-between gap-5 overflow-x-hidden `}
     >
-      {favorites && favorites.length > 0 
-      ? <CardScroller cocktails={favorites} /> 
-      : <h2 className={` text-3xl md:text-4xl lg:text-6xl neon-blue `}>No favorites! Explore some more</h2>}
-      <button
-        role="button"
-        onClick={handleFavoritesCleanup}
-        className=" text-2xl md:text4xl lg:text-6xl neon-button-red neon-button-border "
-      >
-        Clear Favorites
-      </button>
+      {favorites && favorites.length > 0 ? (
+        <CardScroller cocktails={favorites} />
+      ) : (
+        <h2 className={` text-3xl md:text-4xl lg:text-6xl neon-blue `}>
+          No favorites! Explore some more
+        </h2>
+      )}
+      {favorites && favorites.length > 0 && (
+        <button
+          role="button"
+          onClick={handleFavoritesCleanup}
+          className=" text-2xl md:text4xl lg:text-6xl neon-button-red neon-button-border "
+        >
+          Clear Favorites
+        </button>
+      )}
       <div className=" w-full h-0 border-b-4 border-white [box-shadow:1px_0px_200px_10px_#fff]"></div>
     </section>
   );
