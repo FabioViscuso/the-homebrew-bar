3d924bafde72973bd3309197e4f939bbb1a4bf2e
Fabio Viscuso <viscuso.fabio@outlook.it>
2023-08-02 13:11:31 +0200

    feat: move close button to the upper right

--- src/components/dashboard/FullCocktailCard.tsx
+++ src/components/dashboard/FullCocktailCard.tsx
@@ -14,14 +14,23 @@ export default function FullCocktailCard({
   onClick,
 }: CocktailFullCardProps) {
   return (
-    <section className=" fixed top-20 bottom-0 left-0 right-0 z-50 bg-black bg-opacity-60 [backdrop-filter:blur(4px)] flex items-center  overflow-y-scroll ">
-      <div className=" flex flex-col justify-around items-center w-3/4 mx-auto h-full">
-        <h2
-          className={`${sacramento.className} hidden lg:block text-6xl neon-blue `}
+    <section className=" fixed top-20 bottom-0 left-0 right-0 z-50 bg-black bg-opacity-60 [backdrop-filter:blur(4px)] flex items-center overflow-y-scroll ">
+      <div className=" flex flex-col justify-center items-center gap-16 w-3/4 mx-auto h-full ">
+        <div className="flex justify-between items-center w-full px-2">
+          <h2
+            className={`${sacramento.className} hidden lg:block text-6xl neon-blue `}
+          >
+            {cocktail.strDrink}
+          </h2>
+
+          <button
+          className="neon-button-red neon-button-border text-2xl"
+          onClick={onClick}
         >
-          {cocktail.strDrink}
-        </h2>
-        <div className="flex flex-col md:flex-row items-center gap-6">
+          Close
+        </button>
+        </div>
+        <div className="flex flex-col md:flex-row justify-between items-center gap-6 w-full ">
           <Image
             src={cocktail.strDrinkThumb}
             alt={`${cocktail.strDrink}-image`}
@@ -29,7 +38,7 @@ export default function FullCocktailCard({
             width={200}
             className="hidden lg:block rounded-full"
           />
-          <div className="flex flex-col self-start gap-3 border-y-2 lg:border-x-2 lg:border-y-0 border-[#fff] lg:px-6">
+          <div className="flex flex-col gap-3 border-y-2 lg:border-x-2 lg:border-y-0 border-[#fff] lg:px-6 ">
             {Object.keys(cocktail).map((key: string) => {
               if (key.startsWith("strIngredient") && cocktail[key]) {
                 const ingredientIndex = key.substring(13);
@@ -45,16 +54,10 @@ export default function FullCocktailCard({
               return null;
             })}
           </div>
-          <p className="w-full lg:w-[clamp(4rem,40vw,30rem)] text-lg lg:text-2xl">
+          <p className="w-full lg:w-[clamp(4rem,40vw,30rem)] text-lg lg:text-2xl pr-2 overflow-y-scroll ">
             {cocktail.strInstructions}
           </p>
         </div>
-        <button
-          className="neon-button-red neon-button-border text-2xl"
-          onClick={onClick}
-        >
-          Close
-        </button>
       </div>
     </section>
   );
